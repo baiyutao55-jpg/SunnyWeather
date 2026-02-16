c81af971f42712d4a8022be407211ec2f3ddf269
byt <282426833@qq.com>
2026-02-16 13:44:38 +0800

    Sunnyweather commit

--- app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
@@ -11,6 +11,11 @@ import kotlin.coroutines.suspendCoroutine
 object SunnyWeatherNetwork {
     private val placeService= ServiceCreator.create<PlaceService>()
     suspend fun searchPlaces(query: String)=placeService.searchPlaces(query).await()
+
+    private val weatherService= ServiceCreator.create(WeatherService::class.java)
+    suspend fun getDailyWeather(lng: String,lat: String)=weatherService.getDailyWeather(lng,lat).await()
+    suspend fun getRealtimeWeather(lng: String,lat: String)=weatherService.getRealtimeweather(lng,lat).await()
+
     private suspend fun <T> Call<T>.await():T{
         return suspendCoroutine {
             continuation ->
@@ -28,5 +33,9 @@ object SunnyWeatherNetwork {
             )
         }
 
+
     }
+
+
+
 }
